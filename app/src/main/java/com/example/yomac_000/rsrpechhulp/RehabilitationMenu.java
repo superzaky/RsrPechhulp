26012e18ff641ab8f13d38e5f5234e5e84e2fe44
superzaky <superkytoz@gmail.com>
2016-01-02 23:19:18 +0100

    Busy with rehabilitation menu view

---
+++ app/src/main/java/com/example/yomac_000/rsrpechhulp/RehabilitationMenu.java
@@ -0,0 +1,15 @@
+package com.example.yomac_000.rsrpechhulp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by yomac_000 on 2-1-2016.
+ */
+public class RehabilitationMenu extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
