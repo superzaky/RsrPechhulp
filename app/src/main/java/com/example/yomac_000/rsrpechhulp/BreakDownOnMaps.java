99a28502f60963e8e4de9e9dc6a9a4860c808160
superzaky <superkytoz@gmail.com>
2016-01-09 00:23:20 +0100

    Busy with LocationListener bug

--- app/src/main/java/com/example/yomac_000/rsrpechhulp/BreakDownOnMaps.java
+++ app/src/main/java/com/example/yomac_000/rsrpechhulp/BreakDownOnMaps.java
@@ -4,21 +4,30 @@ import android.Manifest;
 import android.annotation.TargetApi;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
+import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-import utils.MyLocationListener;
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
-public class BreakDownOnMaps extends FragmentActivity {
+public class BreakDownOnMaps extends FragmentActivity{
 
     private GoogleMap mMap;
-    MyLocationListener locationListener;
+    LocationListener locationListener;
 
 
     @TargetApi(Build.VERSION_CODES.M)
@@ -33,38 +42,70 @@ public class BreakDownOnMaps extends FragmentActivity {
                 && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
 
         }
+        locationListener = new LocationListener(){
+            public void onLocationChanged(Location loc) {
+                Toast.makeText(
+                        getBaseContext(),
+                        "Location changed: Lat: " + loc.getLatitude() + " Lng: "
+                                + loc.getLongitude(), Toast.LENGTH_SHORT).show();
+                String longitude = "Longitude: " + loc.getLongitude();
+                String latitude = "Latitude: " + loc.getLatitude();
 
-        locationListener = new MyLocationListener(BreakDownOnMaps.this);
+                /*------- To get city name from coordinates -------- */
+                String cityName = null;
+                Geocoder gcd = new Geocoder(getBaseContext(), Locale.getDefault());
+                List<Address> addresses;
+                try {
+                    addresses = gcd.getFromLocation(loc.getLatitude(),
+                            loc.getLongitude(), 1);
+                    if (addresses.size() > 0) {
+                        System.out.println(addresses.get(0).getLocality());
+                        cityName = addresses.get(0).getLocality();
+                    }
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+                String s = longitude + "\n" + latitude + "\n\nMy Current City is: "
+                        + cityName;
+                System.out.println(s);
+                handleNewLocation(loc);
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+
         locationManager.requestLocationUpdates(
                 LocationManager.GPS_PROVIDER, 5000, 10, locationListener);
 
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
     }
 
+    public void handleNewLocation(Location loc) {
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
-//    @TargetApi(Build.VERSION_CODES.M)
-//    @Override
-//    public void onMapReady(GoogleMap googleMap) {
-//        mMap = googleMap;
-//        locationListener = new MyLocationListener(BreakDownOnMaps.this);
-//
-//
-//
-//
-//        // Add a marker in Sydney and move the camera
-//        LatLng sydney = new LatLng(-34, 151);
-////        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-////        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-//    }
+        double currentLatitude = loc.getLatitude();
+        double currentLongitude = loc.getLongitude();
+        LatLng latLng = new LatLng(currentLatitude, currentLongitude);
+
+        System.out.println("handleNewLocation ");
+
+        //mMap.addMarker(new MarkerOptions().position(new LatLng(currentLatitude, currentLongitude)).title("Current Location"));
+        MarkerOptions options = new MarkerOptions()
+                .position(latLng)
+                .title("I am here!");
+        mMap.addMarker(options);
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+    }
 }
